a72e81e55e79749957e413eaeee8d7331160268c
koyahness <cryptomakoya@gmail.com>
2026-01-17 10:33:47 +0200

    update

--- in-tutorial/ArrayDemo.sol
+++ in-tutorial/ArrayDemo.sol
@@ -5,6 +5,27 @@ pragma solidity ^0.8.0;
 contract ArrayDemo {
     uint[] public numbers = [1,2,3,4,5,6,7,8,9,10];
 
+    function _countEvenNumbers() internal view returns(uint) {
+        
+    uint result = 0;
+
+    for(uint i = 0; i < numbers.length; i++) {
+        if(numbers[i] % 2 == 0) {
+            result++;
+        }
+    }
+
+    return result;
+}
+
+
+
+
+
+
+
+
+
     function getEvenNumbers() external view returns(uint[] memory) {
         // TODOhhhhhhhhhhh
     }
