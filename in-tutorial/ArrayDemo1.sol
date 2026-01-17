2b8910b0b3a6bb1b92d8b76fe011715377b5f9ac
koyahness <cryptomakoya@gmail.com>
2026-01-17 11:44:33 +0200

    update

--- in-tutorial/ArrayDemo1.sol
+++ in-tutorial/ArrayDemo1.sol
@@ -3,25 +3,25 @@
 pragma solidity ^0.8.0;
 
 
-contract ArrayDemo {
-    uint[] public numbers = [1,2,3,4,5,6,7,8,9,10];
-    uint numEven;
+// contract ArrayDemo {
+//     uint[] public numbers = [1,2,3,4,5,6,7,8,9,10];
+//     uint numEven;
 
-    function _countEvenNumbers() internal view returns(uint) {
-// You aren’t modifying state, so you should mark it as such: view
-    uint result = 0;
-    for(uint i = 0; i < numbers.length; i++) {
-        if(numbers[i] % 2 == 0) {
-            result++;
-        }
-    }
+//     function _countEvenNumbers() internal view returns(uint) {
+// // You aren’t modifying state, so you should mark it as such: view
+//     uint result = 0;
+//     for(uint i = 0; i < numbers.length; i++) {
+//         if(numbers[i] % 2 == 0) {
+//             result++;
+//         }
+//     }
 
-    return result;
-}
+//     return result;
+// }
 
     function getEvenNumbers() external view returns(uint[] memory) {
         // filter through the array to get even numbers
-        uint resultsLength = _countEvenNumbers();
+        uint resultsLength = numEven;
         uint[] memory evenNumbers_results = new uint[](resultsLength);
         uint cursor = 0;
 
