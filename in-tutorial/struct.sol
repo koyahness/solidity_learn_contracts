a32273de2b82f36e6b51f3d3454c4e7e4d85ae17
koyahness <cryptomakoya@gmail.com>
2026-01-18 11:37:20 +0200

    updates

--- in-tutorial/struct.sol
+++ in-tutorial/struct.sol
@@ -50,6 +50,38 @@ Favorites public myFavorites3 = Favorites(
 
 
 //to store multiple instances
+// use an array, which takes advantage of .push
 
+Favorites[] public userFavorites;
+
+function addFavorite(
+    uint _favoriteNumber,
+    uint _birthDay,
+    string calldata _favoriteColor,
+    uint[5] calldata _lotteryNumbers // Removed the '5' to make dynamic, Dynamic Storage Arrays in Structs
+) public {
+    // .push() returns a reference to the new element
+    Favorites storage newFavorite = userFavorites.push();
+    newFavorite.favoriteNumber = _favoriteNumber;
+    newFavorite.birthDay = _birthDay;
+    newFavorite.favoriteColor = _favoriteColor;
+    newFavorite.lotteryNumbers = _lotteryNumbers;
+}
+
+function addFavorite1(
+    uint _favoriteNumber,
+    uint _birthDay,
+    string calldata _favoriteColor,
+    uint[5] calldata _lotteryNumbers
+) public {
+    Favorites public myFavorites = Favorites(
+        29,
+        14,
+        "red",
+        [uint(1), 2, 3, 4, 5]
+    );
+
+    userFavorites.push(myFavorites);
+}
 
 }
