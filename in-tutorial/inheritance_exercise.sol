60f95c53301de858919fcdae7120616200881b81
koyahness <cryptomakoya@gmail.com>
2026-01-19 16:12:17 +0200

    add inheritance docs

--- in-tutorial/inheritance_exercise.sol
+++ in-tutorial/inheritance_exercise.sol
@@ -40,10 +40,37 @@ contract Salaried is Employee  {
 
 uint public annualSalary;
 
+constructor (uint _annualSalary) {
+
+    annualSalary = _annualSalary;
+
+}
+
 function getAnnualCost() public override view returns (uint) {
 return annualSalary;
 
 }
 
+
+}
+
+
+
+contract Hourly is Employee{
+
+uint public hourlyRate;
+
+uint public hours_time;
+
+
+constructor () {
+
+    hours_time = 2080;
+}
+
+function getAnnualCost() public override view returns (uint) {
+return hourlyRate * hours_time;
+
+}
 
 }
