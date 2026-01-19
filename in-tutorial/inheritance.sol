8ea9b5e260e18c84dfedf9379a1770ecbd2e4358
koyahness <cryptomakoya@gmail.com>
2026-01-19 11:10:13 +0200

    set an inheritance contract

--- in-tutorial/inheritance.sol
+++ in-tutorial/inheritance.sol
@@ -3,3 +3,4 @@
 pragma solidity ^0.8.0;
 
 // Solidity is an object-oriented language. Contracts can inherit from one another, allowing efficient reuse of code.
+// impact inheritance has on the byte code size limit
