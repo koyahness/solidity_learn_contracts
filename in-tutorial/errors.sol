064ef28291f3894943b91d928f196d96e5b9142b
koyahness <cryptomakoya@gmail.com>
2026-01-21 10:58:59 +0200

    add compiler Error

--- in-tutorial/errors.sol
+++ in-tutorial/errors.sol
@@ -5,7 +5,16 @@ pragma solidity ^0.8.0;
 // Compiler Errors
 
 // Bad code example, do not use
+// You will get a compiler error if you try to assign a literal to the wrong type.
 function compilerTypeError() public pure returns (uint) {
     uint myNumber = "One";
     return myNumber;
 }
+
+//compilerTypeError
+// from solidity:
+// TypeError: Type literal_string "One" is not implicitly convertible to expected type uint256.
+//  --> contracts/ErrorTriage.sol:8:9:
+//   |
+// 8 |         uint myNumber = "One";
+//   |         ^^^^^^^^^^^^^^^^^^^^^
