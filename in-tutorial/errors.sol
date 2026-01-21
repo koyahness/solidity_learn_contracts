25c91db6dd50ea635ff204671cd2bddfb231f97d
koyahness <cryptomakoya@gmail.com>
2026-01-21 11:01:36 +0200

    Fix by correcting the type or value, as appropriate for your needs:

--- in-tutorial/errors.sol
+++ in-tutorial/errors.sol
@@ -6,10 +6,11 @@ pragma solidity ^0.8.0;
 
 // Bad code example, do not use
 // You will get a compiler error if you try to assign a literal to the wrong type.
-function compilerTypeError() public pure returns (uint) {
-    uint myNumber = "One";
-    return myNumber;
-}
+
+// function compilerTypeError() public pure returns (uint) {
+//     uint myNumber = "One";
+//     return myNumber;
+// }
 
 //compilerTypeError
 // from solidity:
@@ -18,3 +19,15 @@ function compilerTypeError() public pure returns (uint) {
 //   |
 // 8 |         uint myNumber = "One";
 //   |         ^^^^^^^^^^^^^^^^^^^^^
+
+// Fix by correcting the type or value, as appropriate for your needs:
+//  Fix by correcting the type or value, as appropriate for your needs:
+
+contract errors {
+    
+function compilerTypeErrorFixed() public pure returns (string) {
+    string myNumber = "One";
+    return myNumber;
+}
+
+}
