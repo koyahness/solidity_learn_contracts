44acfc044936c59015f6545469433d2b1dfba696
koyahness <cryptomakoya@gmail.com>
2026-01-21 10:57:28 +0200

    // Bad code example, do not use

--- in-tutorial/errors.sol
+++ in-tutorial/errors.sol
@@ -2,3 +2,10 @@
 
 pragma solidity ^0.8.0;
 
+// Compiler Errors
+
+// Bad code example, do not use
+function compilerTypeError() public pure returns (uint) {
+    uint myNumber = "One";
+    return myNumber;
+}
