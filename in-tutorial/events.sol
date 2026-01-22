916b48975962e13f3e18c6fd052d72b5cdcb104b
koyahness <cryptomakoya@gmail.com>
2026-01-22 16:21:13 +0200

    when you call them, they cause the arguments to be stored in the transaction’s log – a special data structure in the blockchain

--- in-tutorial/events.sol
+++ in-tutorial/events.sol
@@ -4,3 +4,7 @@ pragma solidity ^0.8.0;
 
 //declare, trigger, and utilize events
 //enhance the functionality and user experience of your decentralized applications
+
+// an abstraction on top of the EVM’s logging functionality. Applications can subscribe and listen to these events through the RPC interface of an Ethereum client.
+
+//when you call them, they cause the arguments to be stored in the transaction’s log – a special data structure in the blockchain
