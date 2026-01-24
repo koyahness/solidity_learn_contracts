6d7871d7eadffcc6c636dc1f33233b34e96d6164
koyahness <cryptomakoya@gmail.com>
2026-01-24 16:06:22 +0200

    update claim function

--- in-tutorial/ERC20exercise.sol
+++ in-tutorial/ERC20exercise.sol
@@ -5,25 +5,25 @@ pragma solidity ^0.8.17;
 import "./EnumerableSet.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
-using EnumerableSet for EnumerableSet.AddressSet;
+
 
 contract WeightedVoting is ERC20 {
+
+using EnumerableSet for EnumerableSet.AddressSet;
 
 uint public maxSupply;
 // Issue[] public issues;
 
 constructor {
-    maxSupply = 1,000,000;
+    maxSupply = 1000000;
 }
 
-
-
-error TokensClaimed()
-error AllTokensClaimed()
-error NoTokensHeld()
-error QuorumTooHigh() //returning the quorum amount proposed
-error AlreadyVoted()
-error VotingClosed()
+error TokensClaimed();
+error AllTokensClaimed();
+error NoTokensHeld();
+error QuorumTooHigh(int256 quorum); //returning the quorum amount proposed
+error AlreadyVoted();
+error VotingClosed();
 
 struct Issue {
     EnumerableSet.AddressSet private voters;
@@ -42,21 +42,25 @@ enum Vote { AGAINST, FOR, ABSTAIN }
 Issue[] public issues;
 
 //mint to other users. 
-constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
-    _mint(msg.sender, maxSupply);
-}
+  constructor() ERC20("WeightedVoting", "WV") {
+        // Burn zeroeth element fo issue struct array issue[0] should retrun nothing
+        issues.push();
+    }
 
-
-
+mapping (address => bool) public claimed;
 
 function claim () public {
 uint claimTokens = 100;
+if (claimed[msg.sender] == false){
 
 if (maxSupply >=  claimTokens){
+     _mint(msg.sender, claimTokens);
+    claimed[msg.sender] == true;
+    maxSupply -= claimTokens;
+    }else {AllTokensClaimed()}
 
 }
-
-}
+} else {revert TokensClaimed()}
 
 
 
