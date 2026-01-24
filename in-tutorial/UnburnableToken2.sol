c70448592d256242eb26fc6dce17b33c15b48526
koyahness <cryptomakoya@gmail.com>
2026-01-24 09:55:59 +0200

    update Unburnable token contract

--- in-tutorial/UnburnableToken2.sol
+++ in-tutorial/UnburnableToken2.sol
@@ -3,23 +3,25 @@ pragma solidity ^0.8.19;
 
 contract UnburnableToken {
 
+//variables for token supply, claimed tokens and wallet status: balance and claimed status
 uint public totalSupply;
 uint public totalClaimed;
 mapping (address => uint) public balances;
 mapping (address => bool) public claimed;
 
-// sets total supply
+// sets total supply, 100_000_000; // No decimals, simple integer tokens
 constructor {
     totalSupply = 100,000,000;
 }
 
 //custom errors
 error AllTokensClaimed(uint _totalSupply);
-error TokensClaimed(uint __amountClaimed);
+error TokensClaimed(uint _amountClaimed);
 error UnsafeTransfer(uint _balance, uint _msgvalue);
 error BalanceNotEnough(uint _balance);
 
 
+// claim 1000 tokens per address
 function claim (uint _amountClaimed) public {
     if (claimed[msg.sender] == 0) {
     if (totalSupply >= _amountClaimed){
